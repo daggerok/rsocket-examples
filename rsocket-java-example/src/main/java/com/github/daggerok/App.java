7908e0c5d7cb894f389da735889654184b4410c0
Maksim Kostromin <daggerok@gmail.com>
2019-04-13 01:29:37 +0300

    Fix all the shit...

--- rsocket-java-example/src/main/java/com/github/daggerok/App.java
+++ rsocket-java-example/src/main/java/com/github/daggerok/App.java
@@ -1,14 +1,16 @@
 package com.github.daggerok;
 
 import com.github.daggerok.client.Heroku;
+import org.jboss.weld.environment.se.beans.ParametersFactory;
 
 import javax.enterprise.inject.se.SeContainerInitializer;
 import javax.enterprise.inject.spi.BeanManager;
 
 public class App {
   public static void main(String[] args) {
+    // TODO: new ParametersFactory().setArgs(args);
     SeContainerInitializer.newInstance()
-                          .disableDiscovery()
+                          //.disableDiscovery()
                           .setClassLoader(App.class.getClassLoader())
                           .addPackages(true, BeanManager.class)
                           .addPackages(true, Heroku.class)
--- rsocket-java-example/src/main/java/com/github/daggerok/heroku/Heroku.java
+++ rsocket-java-example/src/main/java/com/github/daggerok/heroku/Heroku.java
@@ -7,18 +7,26 @@ import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import io.rsocket.util.DefaultPayload;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.weld.environment.se.bindings.Parameters;
 import org.jboss.weld.environment.se.events.ContainerInitialized;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Singleton;
 import java.net.URI;
 import java.time.Duration;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
 @Singleton
 public class Heroku {
-  public void on(@Observes ContainerInitialized e) {
+
+  // TODO: @Inject @Parameters List<String> params;
+  // TODO: @Inject @Parameters String[] paramsArray;
+
+  public void on(@Observes ContainerInitialized event, @Parameters List<String> parameters) {
+    // TODO: log.info("bootstrap with parameters {}", parameters);
+
     URI uri = URI.create("ws://rsocket-demo.herokuapp.com/ws");
     WebsocketClientTransport ws = WebsocketClientTransport.create(uri);
     RSocket client = Objects.requireNonNull(
