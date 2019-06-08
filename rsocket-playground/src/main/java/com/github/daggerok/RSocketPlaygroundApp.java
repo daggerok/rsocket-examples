76c1666980f14930db9b56103d8ab8ce5b3e9c02
Maksim Kostromin <daggerok@gmail.com>
2019-06-08 13:23:45 +0300

    remove useless shit | Some shit. :) (C) whatthecommit.com

---
+++ rsocket-playground/src/main/java/com/github/daggerok/RSocketPlaygroundApp.java
@@ -0,0 +1,12 @@
+package com.github.daggerok;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class RSocketPlaygroundApp {
+  public static void main(String[] args) {
+    log.info("please, run tests...");
+  }
+}
