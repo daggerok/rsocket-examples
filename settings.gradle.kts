03b43f453ca5f1c9394bf58a3a856d75ebd8a67e
Maksim Kostromin <daggerok@gmail.com>
2019-12-25 23:00:39 +0200

    /sigh (C) whatthecommit.com

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,13 @@ pluginManagement {
       }
     }
   }
+  val kotlinVersion: String by extra
+  plugins {
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("io.franzbecker.gradle-lombok") version "3.2.0"
+    id("com.github.ben-manes.versions") version "0.27.0"
+  }
 }
 include(
     "rsocket-java-example"
